4ce9cd194fec60f07d22c6b51012209da24162d0
ariful4966 <ariful.practice@gmail.com>
2023-05-24 12:41:45 +0600

    craete register and login page and working successfully

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+
+import React from "react";
+import Form from "./Form";
 
 const Loginpage = () => {
-    return (
-        <div>
-            Login Page
-        </div>
-    );
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("min-width:1000px");
+
+  return (
+    <Box
+      width={"100%"}
+      backgroundColor={theme.palette.background.alt}
+      p="1rem 6%"
+      textAlign={"center"}
+    >
+      <Typography fontWeight={"bold"} fontSize={"32px"} color="primary">
+        SocalNote
+      </Typography>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius={"1.5rem"}
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight={"500"} variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Socialmedia, the Social Media for SocalNotes!
+        </Typography>
+        <Form/>
+      </Box>
+    </Box>
+  );
 };
 
 export default Loginpage;
--- src/state/index.js
+++ src/state/index.js
@@ -15,7 +15,7 @@ export const authSlice = createSlice({
         },
         setLogin:(state, action)=>{
             state.user = action.payload.user;
-            state.toke = action.payload.token;
+            state.token = action.payload.token;
         },
         setLogout:(state)=>{
             state.user = null;
